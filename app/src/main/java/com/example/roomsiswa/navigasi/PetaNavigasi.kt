9ad579d3d44373a1cb2118983998dee7ca188d4b
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-04 20:01:39 +0700

    Menambahkan elemen NavHost

--- app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/PetaNavigasi.kt
@@ -5,16 +5,34 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.roomsiswa.ui.halaman.DestinasiEntry
+import com.example.roomsiswa.ui.halaman.DestinasiHome
+import com.example.roomsiswa.ui.halaman.EntrySiswaScreen
+import com.example.roomsiswa.ui.halaman.HomeScreen
 
 @Composable
 fun SiswaApp(navController: NavHostController= rememberNavController()) {
     HostNavigasi(navController = navController)
 
 }
+@Composable
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-){}
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route,modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
+}
 
 
