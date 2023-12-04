52b268c46ab7eaf8472371779e69a0ffc5e9f01b
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-04 18:31:29 +0700

    Membuat function validasi input

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -4,11 +4,17 @@ import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.repository.RepositorySiswa
 
 class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+
     /*
        *Berisi status Siswa saat ini
         */
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
     }
 }
