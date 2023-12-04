86ff3c1093dafee5b99be6610a096e1379e1432d
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-04 20:14:28 +0700

    Menambahkan SiswaTopAppBar di EntrySiswaScreen

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,5 +1,9 @@
 package com.example.roomsiswa.ui.halaman
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -7,11 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import com.example.roomsiswa.R
 import com.example.roomsiswa.model.EntryViewModel
 import com.example.roomsiswa.model.PenyediaViewModel
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.navigasi.SiswaTopAppBar
+import kotlinx.coroutines.launch
 
 
 object DestinasiEntry: DestinasiNavigasi{
@@ -32,9 +39,10 @@ fun EntrySiswaScreen(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
             )
         }
-    ) {
-
-    }
+    ) {}
 }
