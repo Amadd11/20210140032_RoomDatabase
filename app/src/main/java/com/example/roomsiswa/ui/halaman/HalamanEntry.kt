5a678ceeb95f9a3d2aaab1dfafa60477e957bf25
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-04 19:42:30 +0700

    Menambahkan Function SiswaApp dan HostNavigasi

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,15 +1,18 @@
 package com.example.roomsiswa.ui.halaman
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.example.roomsiswa.R
 import com.example.roomsiswa.model.EntryViewModel
 import com.example.roomsiswa.model.PenyediaViewModel
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 object DestinasiEntry: DestinasiNavigasi{
     override val route = "item_entry"
@@ -25,4 +28,13 @@ fun EntrySiswaScreen(
 ){
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+            )
+        }
+    ) {
+
+    }
 }
