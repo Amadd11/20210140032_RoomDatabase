943f99285ecc08a367782a1fac6d15e6c85d8e20
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-04 18:30:55 +0700

    Membuat variable uiStateSiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -4,6 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.repository.RepositorySiswa
 
 class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    /*
+       *Berisi status Siswa saat ini
+        */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
-
+    }
 }
