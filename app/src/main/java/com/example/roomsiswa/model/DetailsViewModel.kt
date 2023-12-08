b4562d8eb976d3fa3d09f65eb4e7a9bf5cc3d483
Amadd11 <muhammad.syafei.ft21@mail.umy.ac.id>
2023-12-08 11:35:50 +0700

    Menambahkan fitur Detail

---
+++ app/src/main/java/com/example/roomsiswa/model/DetailsViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.roomsiswa.model
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomsiswa.repository.RepositorySiswa
+import com.example.roomsiswa.ui.halaman.DetailsDestination
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+    ) : ViewModel() {
+
+        private val siswaId: Int = checkNotNull(savedStateHandle[DetailsDestination.siswaIdArg])
+        val uiState: StateFlow<ItemDetailsUiState> =
+            repositorySiswa.getSiswaStream(siswaId)
+                .filterNotNull()
+                .map {
+                    ItemDetailsUiState(detailSiswa = it.toDetailSiswa())
+                }.stateIn(
+                    scope = viewModelScope,
+                    started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                    initialValue = ItemDetailsUiState()
+                )
+    suspend fun deleteItem(){
+        repositorySiswa.deleteSiswa(uiState.value.detailSiswa.toSiswa())
+    }
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+data class ItemDetailsUiState(
+    val outOfStock: Boolean = true,
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+)
--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -15,6 +16,12 @@ object PenyediaViewModel {
 
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+        initializer {
+            DetailsViewModel(
+                createSavedStateHandle(),
+                aplikasiSiswa().container.repositorySiswa
+            )
         }
 
     }
